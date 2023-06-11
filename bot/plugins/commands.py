2a21dd752d8884933bd8f1242284b395fff586dd
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2023-06-11 14:23:57 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,22 +1,42 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
-
 from pyrogram import filters, Client, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram.errors import UserNotParticipant
+import random
+
 from bot import Translation, LOGGER # pylint: disable=import-error
 from bot.database import Database # pylint: disable=import-error
-import random
 
 db = Database()
 
-VIDS = [
- "https://telegra.ph/file/e566e33d594c37af8b5b5.mp4"
+force_channel = "crazy_cinemas_official"
+
+PICS = [
+ "https://telegra.ph/file/0a0a44828a9854bab75a7.jpg",
+ "https://telegra.ph/file/00b81ceb39ff1e1128c34.jpg"
+]
+
+VID = [
+ "https://telegra.ph/file/d5082ef18ad41aa35d074.mp4"
 ]
 
 @Client.on_message(filters.command(["start"]) & filters.private, group=1)
 async def start(bot, update):
-    
+    if force_channel:
+        try:
+            user = await bot.get_chat_member(force_channel, update.from_user.id)
+            if user.status == "kicked out":
+                await update.reply_text("You Are Banned")
+                return
+        except UserNotParticipant :
+            await update.reply_text(
+                text="🔊 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 🤭.\n\nനിങ്ങൾക്ക് മൂവീസ് വേണോ? എങ്കിൽ തായെ കാണുന്ന ഞങ്ങളുടെ മെയിൻ ചാനലിൽ ജോയിൻ ചെയ്യുക.😂\nഎന്നിട്ട് ഗ്രൂപ്പിൽ പോയി വീണ്ടും മൂവിയിൽ ക്ലിക് ചെയ്ത് start കൊടുത്തു നോക്കൂ..!😁",
+                reply_markup=InlineKeyboardMarkup( [[
+                 InlineKeyboardButton("🔊 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 🤭", url=f"t.me/{force_channel}")
+                 ]]
+                 )
+            )
+            return
+   
     try:
         file_uid = update.command[1]
     except IndexError:
@@ -27,96 +47,55 @@ async def start(bot, update):
         
         if (file_id or file_type) == None:
             return
-        
+     #CUSTOM FILE CAPTION   
         caption=f"""Hey {update.from_user.mention} 😍
         
-Here is your file..!!
-        
-File Information
------------------
-📁 File Name   : <code> {file_name} </code>
-⚜ File Caption : <code> {file_caption} </code>
-⚙ File Size    : <code> {file_size} </code>
-🗂 File ID      : <code> {file_id} </code>
-🔅 File Type    : <code> {file_type} </code>"""
-       
+<code>{file_name} </code>
+
+⚠ ഈ ഫയൽ ശരിയായി പ്രവർത്തിക്കാൻ ഞങ്ങളുടെ ചാനലിലും ഗ്രൂപ്പിലും ചേരുക..!!
+
+♻️ 𝙅𝙊𝙄𝙉 :- @crazy_cinemas_official
+♻️ 𝙅𝙊𝙄𝙉 :- @crazy_cinemas_group"""
         try:
             await update.reply_cached_media(
                 file_id,
                 quote=True,
                 caption = caption,
-                parse_mode=enums.ParseMode.HTML,
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton
-                                (
-                                    'Developers', url="https://t.me/crazy_cinemas_official"
-                                )
-                        ]
-                    ]
+                reply_markup=InlineKeyboardMarkup( [[
+                    InlineKeyboardButton("🔰 SUPPORT GROUP 🔰", url="t.me/crazy_cinemas_group"),
+                    ],[
+                    InlineKeyboardButton("💠 SUPPORT CHANNEL 💠", url="https://t.me/crazy_cinemas_official")
+                    ]]
+                    )
                 )
-            )
         except Exception as e:
             await update.reply_text(f"<b>Error:</b>\n<code>{e}</code>", True, parse_mode=enums.ParseMode.HTML)
             LOGGER(__name__).error(e)
         return
-
+#pmstart
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
-    ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
-    ],[
-        InlineKeyboardButton('Help ⚙', callback_data="help")
-    ]]
+                    InlineKeyboardButton("🔰 SUPPORT GROUP 🔰", url="t.me/crazy_cinemas_group")
+                ],[
+                    InlineKeyboardButton("💠 SUPPORT CHANNEL 💠", url="https://t.me/crazy_cinemas_official")
+           ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(
-                update.from_user.first_name),
+    await update.reply_photo(
+        photo=random.choice(PICS),
+        caption=Translation.START_TEXT.format(update.from_user.first_name),
         reply_markup=reply_markup,
-        parse_mode=enums.ParseMode.HTML,
         reply_to_message_id=update.id
     )
+    await update.reply_video(
+        video=random.choice(VID),
+        caption=f"""Hey {update.from_user.mention},
+        
+Here is my kiss for my dear... 💋
+Go to Group for Movies...!!
 
-
-@Client.on_message(filters.command(["help"]) & filters.private, group=1)
-async def help(bot, update):
-    buttons = [[
-        InlineKeyboardButton('Home ⚡', callback_data='start'),
-        InlineKeyboardButton('About 🚩', callback_data='about')
-    ],[
-        InlineKeyboardButton('Close 🔐', callback_data='close')
-    ]]
-    
-    reply_markup = InlineKeyboardMarkup(buttons)
-    
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.HELP_TEXT,
-        reply_markup=reply_markup,
-        parse_mode=enums.ParseMode.HTML,
-        reply_to_message_id=update.id
+@crazy_cinemas_official"""
     )
-
-
-@Client.on_message(filters.command(["about"]) & filters.private, group=1)
-async def about(bot, update):
-    
-    buttons = [[
-        InlineKeyboardButton('Home ⚡', callback_data='start'),
-        InlineKeyboardButton('Close 🔐', callback_data='close')
-    ]]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.ABOUT_TEXT,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
-        parse_mode=enums.ParseMode.HTML,
-        reply_to_message_id=update.id
-    )
+     
+     
+     
